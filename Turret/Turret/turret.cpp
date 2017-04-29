98c1ccf3dc0a446723cf80bf11b7b06b1f6b208a
YoungHJ203 <younghj203@gmail.com>
2017-04-29 21:15:22 +0900

    0429_2

--- Turret/Turret/turret.cpp
+++ Turret/Turret/turret.cpp
@@ -3,5 +3,11 @@ using namespace std;
 
 int main() {
 
+	int caseNum;		// the number of cases to test
+	int x1, x2, y1, y2, r1, r2;
+
+	cin >> caseNum;
 	
+	cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
+
 }
