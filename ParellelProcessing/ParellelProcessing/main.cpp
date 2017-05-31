95a45a7839bcb55743cbfd6f95b5c7917f7e9e44
YoungHJ203 <YoungHJ203@gmail.com>
2017-06-01 01:07:04 +0900

    170601

--- ParellelProcessing/ParellelProcessing/main.cpp
+++ ParellelProcessing/ParellelProcessing/main.cpp
@@ -2,8 +2,6 @@
 
 using namespace std;
 
-void cal(int, int, int&);
-
 int main() {
 	int testCase=0;
 
@@ -17,36 +15,18 @@ int main() {
 
 		cin >> a >> b;
 
-		a = a % 10;
-
-		cal(a, b, num);
-		
-		if (num == 1) {
-			b = 1;
-		}
-
-		while (b>num) {
-			b -= num;
-		}
-
 		for (int i = 0; i < b; i++) {
 			result *= a;
+			result = result % 10;
 		}
 
-		cout << result % 10 << endl;		
-	}
-}
-
-// calculate a^b and the number of cycles needed for conditions where units digit of a^b equals to a
-void cal(int a, int b, int &num) {
-	int result = a*a;
-	int remain = result % 10;
-
-	while (remain!=a) {
-		result *= a;
-		remain = result % 10;
-	
 		
-		num++;
+		if (result != 0) {
+			cout << result % 10 << endl;
+		}
+		else {
+			cout << 10 << endl;
+		}
+		
 	}
 }
